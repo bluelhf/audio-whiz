df1131cc98e878068da6ef9e0d9702eda240d64e
bluelhf <blueloveshisfriends@gmail.com>
2022-10-01 15:35:25 +0300

    chore: compress hotel california
    such a lovely place

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ struct Model {
 
 fn model(_app: &App) -> Model {
     let manager = AudioManager::new();
-    manager.play(audrey::read::open(Path::new("./music/hotel_california.wav"))
+    manager.play(audrey::read::open(Path::new("./music/hotel_california.ogg"))
             .expect("failed to read audio")).expect("failed to play audio");
     Model { manager }
 }
